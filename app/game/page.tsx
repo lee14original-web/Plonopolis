eb07f3b57447c40d5f35bbc363620881266729f8
lee14original-web <lee14original@gmail.com>
2026-03-22 22:27:04 +0100

    Create page.tsx

---
+++ app/game/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import FarmView from "@/components/FarmView";
+import { getFarmStage } from "@/config/farmStages";
+
+export default function Game() {
+  const [lvl, setLvl] = useState(1);
+  const stage = getFarmStage(lvl);
+
+  return (
+    <main style={{ padding: 20 }}>
+      
+      <h1>Plonopolis - Farma</h1>
+
+      <div style={{ marginBottom: 20 }}>
+        {[1,5,10,15,20].map(l => (
+          <button key={l} onClick={() => setLvl(l)}>
+            LV {l}
+          </button>
+        ))}
+      </div>
+
+      <FarmView image={stage.image} />
+    </main>
+  );
+}
