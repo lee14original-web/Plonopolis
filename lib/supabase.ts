7fdb32a122002e4d63b0f120799cadb0c8e0417a
lee14original-web <lee14original@gmail.com>
2026-03-24 00:10:43 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://wvgfoevjljggsmgqacg.supabase.co";
+const supabaseAnonKey = "TU_WKLEJ_SWÓJ_ANON_KEY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
