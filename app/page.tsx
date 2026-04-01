cd9792f20bd2adca8d1fab0e42e01e70d725e994
lee14original-web <lee14original@gmail.com>
2026-04-01 18:45:56 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2138,8 +2138,8 @@ export default function Page() {
                       title="Do miasta"
                       className="pointer-events-auto absolute transition-all duration-300 hover:scale-105 hover:-translate-y-1"
                       style={{
-                        left: "8%",
-                        top: "52%",
+                        left: "4%",
+                        top: "49%",
                         width: "12%",
                         height: "16%",
                         zIndex: 20,
