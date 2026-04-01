0dce486456c9a95178fd1103bacc31415cac2251
lee14original-web <lee14original@gmail.com>
2026-04-01 18:04:41 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2059,8 +2059,8 @@ export default function Page() {
     }}
     className="pointer-events-auto absolute transition-all duration-300 hover:scale-[1.02] hover:-translate-y-1"
     style={{
-      left: "50%",
-      top: "61%",
+      left: "51%",
+      top: "60%",
       width: "38%",
       height: "22%",
       zIndex: 4,
